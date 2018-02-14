27f7a67908713ec6fdcf1da8dea306e4ded28501
Miguel Mota <miguelmota2@gmail.com>
2018-02-13 20:05:31 -0800

    update example

--- examples/example.py
+++ examples/example.py
@@ -1,8 +1,6 @@
 import etherdelta
 
+account = '0x85E4B84D784eE9eEB7489F0B0c66B343AF2a0BE5'
 client = etherdelta.Client()
-account = "0x85E4B84D784eE9eEB7489F0B0c66B343AF2a0BE5"
-
-client = etherdelta.Client()
-bal = client.get_eth_balance(account)
-print(bal)
+balance = client.get_eth_balance(account)
+print(balance)
